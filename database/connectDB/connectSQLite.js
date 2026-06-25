b16dc0e31d187e0b8ddaf1b8bad271d6fa08b606
mdajmaul <mdajmaulislam00@gmail.com>
2026-06-25 17:06:56 +0600

    Update connectSQLite.js

--- database/connectDB/connectSQLite.js
+++ database/connectDB/connectSQLite.js
@@ -7,12 +7,20 @@ module.exports = async function () {
 		logging: false
 	});
 
-	const threadModel = require("../models/sqlite/thread.js")(sequelize);
-	const userModel = require("../models/sqlite/user.js")(sequelize);
-	const dashBoardModel = require("../models/sqlite/userDashBoard.js")(sequelize);
-	const globalModel = require("../models/sqlite/global.js")(sequelize);
+	const DataModel = require("../DataModel.js");
+
+	const rawThreadModel = require("../models/sqlite/thread.js")(sequelize);
+	const rawUserModel = require("../models/sqlite/user.js")(sequelize);
+	const rawDashBoardModel = require("../models/sqlite/userDashBoard.js")(sequelize);
+	const rawGlobalModel = require("../models/sqlite/global.js")(sequelize);
 
 	await sequelize.sync({ force: false });
+
+	// Wrap raw Sequelize models so they expose .get()/.set()/.getAll()/.create()/.delete()
+	const threadModel = new DataModel(rawThreadModel);
+	const userModel = new DataModel(rawUserModel);
+	const dashBoardModel = new DataModel(rawDashBoardModel);
+	const globalModel = new DataModel(rawGlobalModel);
 
 	return {
 		threadModel,
